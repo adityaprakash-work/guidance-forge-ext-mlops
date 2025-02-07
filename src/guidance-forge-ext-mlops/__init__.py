cffea9cb6b4a65330f4a00c32dae0a4736e84c21
adityaprakash-work <adityaprakash.work@gmail.com>
2025-02-08 04:56:57 +0530

    🔧 corrected package name

--- src/guidance-forge-ext-mlops/__init__.py
+++
@@ -1,7 +0,0 @@
-# ---INFO-------------------------------------------------------------------------------
-"""AI assisted control for MLOps pipelines"""
-
-# ---DEPENDENCIES-----------------------------------------------------------------------
-from . import agents, settings, utils
-
-__all__ = ["agents", "settings", "utils"]
